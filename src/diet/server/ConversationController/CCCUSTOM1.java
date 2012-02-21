c5bfd0652807d5e3b18e75917dbc53425cd12c4f
Noah Liebman <noah@noahliebman.com>
2012-02-21 12:07:11 -0600

    turned off 'really' removal because the linguistic huristic wasn't right; made sure caps between asterisks get dealt with, made lowercasing shouting better

--- src/diet/server/ConversationController/CCCUSTOM1.java
+++ src/diet/server/ConversationController/CCCUSTOM1.java
@@ -80,16 +80,17 @@ public class CCCUSTOM1 extends DefaultConversationController{
            
            // get rid of asterisks around *emphasized* words
            //mct.setText(mct.getText().replaceAll("(\\*)([a-z]+)(\\*)", mct.getText().split("(\\*)([a-z]+)(\\*)")[1]));
-           mct.setText(mct.getText().replaceAll("(\\*)([a-z]+)(\\*)", "$2"));
+           mct.setText(mct.getText().replaceAll("(\\*)([a-zA-Z]+)(\\*)", "$2"));
            
            // strip "really" when not at the end of a sentence.
-           mct.setText(mct.getText().replaceAll("really\\s", ""));
+           // mct.setText(mct.getText().replaceAll("really\\s", ""));
            
            // If there are 2 caps in a row, make the whole thing lowercase
-           if(mct.getText().matches(".*[A-Z]{2,}.*"))
+           /*if(mct.getText().matches(".*[A-Z]{2,}.*"))
            {
                mct.setText(mct.getText().toLowerCase());
-           }
+           }*/
+           mct.setText(mct.getText().replaceAll("([A-Z]{2,})", "$1".toLowerCase()));
            
            /*try {
                Thread.sleep(3000);
